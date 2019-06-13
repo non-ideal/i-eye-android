3487391e5b3335e8ea0fa9b23efcb5efc7ef7c42
sovovy <sovovy@gmail.com>
2019-06-14 02:24:59 +0900

    loading gif 추가

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchingForLocationActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchingForLocationActivity.kt
@@ -2,12 +2,17 @@ package com.maas.soft.i_eye.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.maas.soft.i_eye.R
+import kotlinx.android.synthetic.main.activity_searching_for_location.*
 
 class SearchingForLocationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searching_for_location)
+
+        Glide.with(this).asGif().load(R.raw.loading).into(iv_loading_searching)
     }
 }
