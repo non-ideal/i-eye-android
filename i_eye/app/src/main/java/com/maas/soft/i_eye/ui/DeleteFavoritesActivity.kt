09ef5f48f57ef7df963c03349563fc79e42302c9
sovovy <sovovy@gmail.com>
2019-06-21 05:55:02 +0900

    <즐겨찾기 삭제> 버튼 연결

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/DeleteFavoritesActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/DeleteFavoritesActivity.kt
@@ -1,13 +1,27 @@
 package com.maas.soft.i_eye.ui
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.maas.soft.i_eye.R
+import kotlinx.android.synthetic.main.activity_delete_favorites.*
 
 class DeleteFavoritesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delete_favorites)
+        setClickListenrs()
+    }
+
+    private fun setClickListenrs(){
+        btn_yes_delete_favorites.setOnClickListener {
+            startActivity(Intent(this, FavoritesActivity::class.java))
+            finish()
+        }
+        btn_no_delete_favorites.setOnClickListener {
+            startActivity(Intent(this, FavoritesActivity::class.java))
+            finish()
+        }
     }
 }
