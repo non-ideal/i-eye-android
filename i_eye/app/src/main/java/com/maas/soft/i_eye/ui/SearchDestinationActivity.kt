aaa2307599496947131e257ac6e3af9a6b4fb8c6
sovovy <sovovy@gmail.com>
2019-06-22 00:38:19 +0900

    <목적지 검색> 아이템 추가, 클릭 리스너

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchDestinationActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchDestinationActivity.kt
@@ -5,11 +5,22 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_search_destination.*
 import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import com.maas.soft.i_eye.R
 
 
-class SearchDestinationActivity : AppCompatActivity() {
+class SearchDestinationActivity : AppCompatActivity() , View.OnClickListener {
+    private val placeItems = ArrayList<String>()
+    private lateinit var placeAdapter: PlaceAdapter
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, CheckDestinationActivity::class.java)
+        intent.putExtra("Destination", placeItems[rv_search_dest_search.getChildAdapterPosition(v!!)])
+        startActivity(intent)
+        finish()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +29,14 @@ class SearchDestinationActivity : AppCompatActivity() {
         imm.hideSoftInputFromWindow(et_search_dest_search.windowToken, 0)
 
         setClickListener()
+
+        placeItems.add("광화문역")
+        placeItems.add("광흥창역")
+        placeAdapter = PlaceAdapter(placeItems)
+        placeAdapter.setOnItemClickListener(this)
+
+        rv_search_dest_search.layoutManager = LinearLayoutManager(this)
+        rv_search_dest_search.adapter = placeAdapter
     }
 
     private fun setClickListener() {
