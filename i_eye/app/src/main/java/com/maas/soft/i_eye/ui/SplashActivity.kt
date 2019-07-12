9ccca30877a61f8d193691eb6182c6e42e28f01b
HyeranShin <hyeran9712@naver.com>
2019-07-12 14:57:11 +0900

    단말기 고유 값 구해서 SharedPreference 저장

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SplashActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SplashActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.os.Handler
 import com.maas.soft.i_eye.R
 import android.content.Intent
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import android.provider.Settings
+import java.util.*
 
 
 class SplashActivity : AppCompatActivity() {
@@ -13,8 +17,23 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        checkFirstRun()
         changeStatusBarColor()
         moveToNextActivity()
+
+    }
+
+    private fun checkFirstRun() {
+        var mPref : SharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+        var firstRun = mPref.getBoolean("firstRun", true)
+        if(firstRun) {
+            var ANDROID_ID : String = Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ANDROID_ID)
+            //TODO 서버로 ANDROID_ID 전송
+            var editor : SharedPreferences.Editor = mPref.edit();
+            editor.putBoolean("firstRun", false)
+            editor.commit()
+        }
     }
 
     private fun changeStatusBarColor() {
