cab21c97f89423a70d4df51963b63ec1a7790796
HyeranShin <hyeran9712@naver.com>
2019-07-14 22:28:09 +0900

    현재 위치 위도, 경도 구하기, 주소로 변환해서 넘기기

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/CheckCurrentLocationActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/CheckCurrentLocationActivity.kt
@@ -3,19 +3,30 @@ package com.maas.soft.i_eye.ui
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.maas.soft.i_eye.R
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_check_current_location.*
 
 class CheckCurrentLocationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_check_current_location)
+
+        setContentView(com.maas.soft.i_eye.R.layout.activity_check_current_location)
         setClickListener()
+        setCurrentLocationText()
+    }
+
+    private fun setCurrentLocationText() {
+        var currentLocation : String = intent.getStringExtra("currentLocation")
+        tv_current_location.text = currentLocation
     }
 
     private fun setClickListener() {
         btn_yes_check_current_location.setOnClickListener {
+            //TODO 서버로 현재 위치 위도, 경도 보내기
+            var latitude : Double = intent.getDoubleExtra("latitude", 0.0)
+            var longitude : Double = intent.getDoubleExtra("longitude", 0.0)
+
             val intent = Intent(applicationContext, SearchDestinationActivity::class.java)
             startActivity(intent)
         }
@@ -25,4 +36,5 @@ class CheckCurrentLocationActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 }
--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchingForLocationActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchingForLocationActivity.kt
@@ -1,14 +1,32 @@
 package com.maas.soft.i_eye.ui
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v4.app.ActivityCompat
+import android.util.Log
 import com.bumptech.glide.Glide
 import com.maas.soft.i_eye.R
+import kotlinx.android.synthetic.main.activity_check_current_location.*
 import kotlinx.android.synthetic.main.activity_searching_for_location.*
+import java.io.IOException
+import java.util.*
 
 class SearchingForLocationActivity : AppCompatActivity() {
+
+    var locationManager : LocationManager? = null
+    private val REQUEST_CODE_LOCATION : Int = 2
+    var currentLocation : String = ""
+    var latitude : Double? = null
+    var longitude : Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +34,54 @@ class SearchingForLocationActivity : AppCompatActivity() {
 
         Glide.with(this).asGif().load(R.raw.loading).into(iv_loading_searching)
 
+        getCurrentLoc()
         moveToNextActivity()
     }
 
+    private fun getCurrentLoc() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        var userLocation: Location = getLatLng()
+        if (userLocation != null) {
+            latitude = userLocation.latitude
+            longitude = userLocation.longitude
+            Log.d("CheckCurrentLocation", "현재 내 위치 값: $latitude, $longitude")
+
+            var mGeocoder = Geocoder(applicationContext, Locale.KOREAN)
+            var mResultList: List<Address>? = null
+            try {
+                mResultList = mGeocoder.getFromLocation(
+                        latitude!!, longitude!!, 1
+                )
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            if (mResultList != null) {
+                Log.d("CheckCurrentLocation", mResultList[0].getAddressLine(0))
+                currentLocation = mResultList[0].getAddressLine(0)
+                currentLocation = currentLocation.substring(11)
+            }
+        }
+    }
+
+    private fun getLatLng() : Location {
+        var currentLatLng: Location? = null
+        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), this.REQUEST_CODE_LOCATION)
+            getLatLng()
+        } else {
+            val locationProvider = LocationManager.GPS_PROVIDER
+            currentLatLng = locationManager?.getLastKnownLocation(locationProvider)
+        }
+        return currentLatLng!!
+    }
+
     private fun moveToNextActivity() {
-        val handler = Handler()
-        handler.postDelayed({
             val intent = Intent(applicationContext, CheckCurrentLocationActivity::class.java)
+            intent.putExtra("currentLocation", currentLocation)
+            intent.putExtra("latitude", latitude)
+            intent.putExtra("longitude", longitude)
             startActivity(intent)
             finish()
-        }, 1500)
     }
 }
