5dad569eaa756be416b615bc9547a4e2f4d225dd
HyeranShin <hyeran9712@naver.com>
2019-07-17 01:58:35 +0900

    티맵 마커, 시점 현재 위치로 지정

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/DirectionsActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/DirectionsActivity.kt
@@ -1,8 +1,16 @@
 package com.maas.soft.i_eye.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.util.Log
 import com.maas.soft.i_eye.R
 import com.skt.Tmap.TMapView
 import kotlinx.android.synthetic.main.activity_directions.*
@@ -10,15 +18,21 @@ import kotlinx.android.synthetic.main.activity_directions.*
 
 class DirectionsActivity : AppCompatActivity() {
 
+    private val REQUEST_CODE_LOCATION : Int = 2
+    private var latitude : Double? = null
+    private var longitude : Double? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_directions)
         changeStatusBarColor()
+        getLatLng()
 
         val tMapView = TMapView(this)
         tMapView.setSKTMapApiKey("767dc065-35e7-4782-a787-202f73d8d976")
-        tMapView.setLocationPoint(126.9840825,37.5670949)
-        tMapView.setCenterPoint(126.9840825,37.5670949)
+        Log.d("@@@", "$latitude, $longitude")
+        tMapView.setLocationPoint(longitude!!,latitude!!)
+        tMapView.setCenterPoint(longitude!!,latitude!!)
         tMapView.setCompassMode(false)
         tMapView.setIconVisibility(true)
         tMapView.zoomLevel = 17
@@ -28,6 +42,24 @@ class DirectionsActivity : AppCompatActivity() {
         tMapView.setSightVisible(false)
         tMapView.contentDescription = "지도 영역입니다"
         mapview_directions.addView(tMapView)
+    }
+
+    private fun getLatLng() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var currentLatLng : Location? = null
+
+        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), this.REQUEST_CODE_LOCATION)
+            getLatLng()
+        } else {
+            val locationProvider = LocationManager.GPS_PROVIDER
+            currentLatLng = locationManager?.getLastKnownLocation(locationProvider)
+        }
+        currentLatLng?.let {
+            latitude = currentLatLng.latitude
+            longitude = currentLatLng.longitude
+        }
     }
 
     private fun changeStatusBarColor() {
