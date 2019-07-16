5504e48163cece3311cc877f883a11e22a515723
sovovy <sovovy@gmail.com>
2019-07-17 06:52:08 +0900

    searching destination by TMAP API

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchDestinationActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/SearchDestinationActivity.kt
@@ -6,18 +6,27 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_search_destination.*
 import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import com.maas.soft.i_eye.R
+import com.skt.Tmap.TMapData
+import com.skt.Tmap.TMapPOIItem
 
 
 class SearchDestinationActivity : AppCompatActivity() , View.OnClickListener {
     private val placeItems = ArrayList<String>()
     private lateinit var placeAdapter: PlaceAdapter
+    private val tmapdata = TMapData()
+    var item : ArrayList<TMapPOIItem> = ArrayList()
 
     override fun onClick(v: View?) {
         val intent = Intent(this, CheckDestinationActivity::class.java)
         intent.putExtra("Destination", placeItems[rv_search_dest_search.getChildAdapterPosition(v!!)])
+
+        // TODO save destination coord
+        // item[rv_search_dest_search.getChildAdapterPosition(v)].poiPoint
+
         startActivity(intent)
         finish()
     }
@@ -30,11 +39,8 @@ class SearchDestinationActivity : AppCompatActivity() , View.OnClickListener {
 
         setClickListener()
 
-        placeItems.add("광화문역")
-        placeItems.add("광흥창역")
         placeAdapter = PlaceAdapter(placeItems)
         placeAdapter.setOnItemClickListener(this)
-
         rv_search_dest_search.layoutManager = LinearLayoutManager(this)
         rv_search_dest_search.adapter = placeAdapter
     }
@@ -43,6 +49,17 @@ class SearchDestinationActivity : AppCompatActivity() , View.OnClickListener {
         tv_voice_dest_search.setOnClickListener {
             val intent = Intent(applicationContext, DestinationVoiceInputActivity::class.java)
             startActivity(intent)
+        }
+        et_search_dest_search.setOnKeyListener { v, keyCode, event ->
+            // press enter
+            if ((event.action == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                item = tmapdata.findAllPOI(et_search_dest_search.text.toString())
+                for (i in 0 until item.size){
+                    placeItems.add(item[i].poiName)
+                }
+                placeAdapter.notifyDataSetChanged()
+            }
+            true
         }
     }
 }
