6a27e1d419bb71ef99a83e4b1f0343b3d12a1714
sovovy <sovovy@gmail.com>
2019-07-18 04:56:29 +0900

    <목적지 음성 검색> 매끄럽게 수정. 앱 이름 안 나오던 에러 수정.

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/CheckDestinationVoiceInputActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/CheckDestinationVoiceInputActivity.kt
@@ -12,5 +12,6 @@ class CheckDestinationVoiceInputActivity : AppCompatActivity() {
         setContentView(R.layout.activity_check_destination_voice_input)
 
         tv_dest_check_dest_voice.text = "${intent.getStringExtra("VOICE_DEST")}\n맞습니까?"
+        tv_dest_check_dest_voice.contentDescription = "${intent.getStringExtra("VOICE_DEST")} 맞습니까?"
     }
 }
