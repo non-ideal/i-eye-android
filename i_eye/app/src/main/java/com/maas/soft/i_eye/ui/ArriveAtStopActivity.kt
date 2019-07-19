b60077a6d0fae9b1f832cf91806a35fbfaf75def
HyeranShin <hyeran9712@naver.com>
2019-07-19 15:04:04 +0900

    뷰 변경된 부분 수정

--- i_eye/app/src/main/java/com/maas/soft/i_eye/ui/ArriveAtStopActivity.kt
+++ i_eye/app/src/main/java/com/maas/soft/i_eye/ui/ArriveAtStopActivity.kt
@@ -11,6 +11,8 @@ class ArriveAtStopActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arrive_at_stop)
         changeStatusBarColor()
+
+        //TODO 지금이 되면 tv_guide_arrive_at_stop 내용 바껴야함
     }
 
     private fun changeStatusBarColor() {
